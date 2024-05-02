9087c74feb74bae3ab30440408ffdfaf0ab677af
Вовчик <sizikoffps@gmail.com>
2024-05-03 02:55:35 +0300

    Добавлен SplashScreen

--- app/src/main/java/com/amicus/letswatch/MainActivity.kt
+++ app/src/main/java/com/amicus/letswatch/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.amicus.letswatch.navigation.SetupNavHost
 import com.amicus.letswatch.ui.theme.LetsWatchTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +21,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LetsWatchTheme {
-
+                val navController = rememberNavController()
+                SetupNavHost(navController = navController)
             }
         }
     }
---
+++ app/src/main/java/com/amicus/letswatch/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.amicus.letswatch.utils
+
+class Constants {
+
+    object Screen{
+        const val SPLASH_SCREEN = "splash_screen"
+        const val MAIN_SCREEN = "main_screen"
+        const val DETAIL_SCREEN = "detail_screen"
+    }
+}
